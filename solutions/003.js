1e5763ef17ea55fa6e15d1dc73d2ef1a31c4cfba
Aleh Kapusta <118632628+Dj-Rom@users.noreply.github.com>
2025-06-19 21:36:52 +0200

    Create 003.js

---
+++ solutions/003.js
@@ -0,0 +1,73 @@
+function fetchVideoDetails(videoId) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const success = Math.random() < 0.9;
+      if (success) {
+        resolve({
+          title: 'Глубокое погружение в асинхронный JavaScript',
+          description: 'Разбираем event loop, промисы и async/await на пальцах.',
+        });
+      } else {
+        reject(new Error('Не удалось загрузить детали видео'));
+      }
+    }, 1 * 1000) //1000ms;
+  });
+}
+
+function fetchComments(videoId) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const success = Math.random() < 0.7;
+      if (success) {
+        resolve([
+          'Отличное объяснение! Наконец-то понял event loop.',
+          'А как насчет Web Workers?',
+          'Лайк за async/await!',
+        ]);
+      } else {
+        reject(new Error('Не удалось загрузить комментарии'));
+      }
+    }, 1.5 *1000); //1000ms
+  });
+}
+
+function fetchRelatedVideos(videoId) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const success = Math.random() < 0.95;
+      if (success) {
+        resolve([
+          'Что такое замыкания в JS?',
+          'Паттерны проектирования для начинающих',
+        ]);
+      } else {
+        reject(new Error('Не удалось загрузить похожие видео'));
+      }
+    }, 0.8 * 1000); //1000ms
+  });
+}
+
+
+
+async function loadVideoPage(videoId) {
+  console.log(`Загрузка страницы для видео ${videoId}...`);
+  try {
+    const [details, comments, related] = await Promise.all([
+      fetchVideoDetails(videoId), 
+      fetchComments(videoId), 
+      fetchRelatedVideos(videoId) 
+    ]);
+
+    console.log('--- Страница загружена полностью ---');
+    console.log('Детали:', details);
+    console.log('Комментарии:', comments);
+    console.log('Похожие видео:', related);
+  } catch (error) {
+  
+    console.error('--- Ошибка загрузки части страницы ---');
+    console.error('Причина:', error.message);
+
+  }
+}
+
+loadVideoPage(1)
