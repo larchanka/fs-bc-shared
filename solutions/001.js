702bb867ae02f07612a44f07a7c5d984b1e59f7c
Alexey Gribov <90611934+AlexCAP38@users.noreply.github.com>
2025-06-20 22:20:57 +0700

    Create 001.js

---
+++ solutions/001.js
@@ -0,0 +1,40 @@
+function fetchUserInfo() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        name: 'Алекс Алгоритмов',
+        bio: 'Строю будущее, по одному циклу за раз.',
+      });
+    }, 1000);
+  });
+}
+
+function fetchUserTweets() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(['Коммичу в пятницу вечером. Что может пойти не так?', 'Баг или фича? 🤔 #программирование', 'Рефакторинг старого кода - это как археология.']);
+    }, 1500);
+  });
+}
+
+function fetchUserFollowers() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(15000);
+    }, 500);
+  });
+}
+
+async function loadUserProfile() {
+  const userInfo = await fetchUserInfo();
+   const userTweets = await fetchUserTweets()
+   const userFollowers = await fetchUserFollowers()
+
+  console.log('Имя:', userInfo.name);
+  console.log('Био:', userInfo.bio);
+  console.log('Твиты:', userTweets.toString());
+  console.log('Подписки:', userFollowers);
+
+}
+
+loadUserProfile();
