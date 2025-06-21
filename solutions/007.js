31d9d83aae43ec446c0f6410305cee76050ae658
Andrey7425 <andrey777mail@yandex.ru>
2025-06-21 20:23:49 +0300

    Update 007.js

--- solutions/007.js
+++ solutions/007.js
@@ -2,3 +2,9 @@ console.log("Начало загрузки страницы видео");
 new Promise(resolve => resolve()).then(res => console.log("Основная информация о видео загружена"))
 setTimeout(()=> console.log("Список рекомендаций загружен"), 0);
 console.log("Конец скрипта")
+
+console.log("Начало загрузки страницы видео");
+setTimeout(()=> console.log("Список рекомендаций загружен"), 0);
+new Promise(resolve => resolve()).then(res => console.log("Основная информация о видео загружена"))
+console.log("Конец скрипта")
+
