8c5f8b1f9ef68d542f61f3c27607fe6e9a55c5e3
Boris Yuzhakov <yuzhakov.boris@gmail.com>
2025-06-21 17:52:26 +0300

    task: 005

--- solutions/utils.ts
+++ solutions/utils.ts
@@ -1,4 +1,5 @@
 export const resolvedPromise = <T,>(data: T, delay: number): Promise<T> => new Promise((rs) => setTimeout(() => rs(data), delay))
+export const rejectedPromise = (message: string, delay: number): Promise<T> => new Promise((_, rj) => setTimeout(() => rj(new Error(message)), delay))
 
 export const schrodingerPromise = (data: unknown, delay: number, errorMessage, successRate = 0.9) => new Promise((rs, rj) => {
   return setTimeout(() => Math.random() <= successRate ? rs(data) : rj(new Error(errorMessage)), delay)
