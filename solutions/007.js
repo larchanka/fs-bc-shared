cf8ec11d8373df777e5abb25edb4229aafd2f82d
tiditik <69196938+tiditik@users.noreply.github.com>
2025-06-22 12:39:15 +0300

    Delete solutions/007.js

--- solutions/007.js
+++
@@ -1,9 +0,0 @@
-console.log("Начало загрузки страницы видео");
-
-
-new Promise((resolve, reject)=>{
-    resolve("Основная информация о видео загружена")
-}).then(res => console.log(res));
-
-setTimeout(()=>{console.log("Список рекомендаций загружен")}, 0);
-setTimeout(()=> {console.log("Конец скрипта")}, 0)
