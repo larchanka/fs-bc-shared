7e65ad91b8851aad2ff4c5572306e2dca9770d8a
tiditik <69196938+tiditik@users.noreply.github.com>
2025-06-22 12:38:45 +0300

    Update 007.js

--- solutions/007.js
+++ solutions/007.js
@@ -1,1 +1,9 @@
-Create 007.js
+console.log("Начало загрузки страницы видео");
+
+
+new Promise((resolve, reject)=>{
+    resolve("Основная информация о видео загружена")
+}).then(res => console.log(res));
+
+setTimeout(()=>{console.log("Список рекомендаций загружен")}, 0);
+setTimeout(()=> {console.log("Конец скрипта")}, 0)
