21ea039852547b5ef62a6fbf319e7dc7d78a1db4
Kekht <reader_af@mail.ru>
2025-06-22 22:59:10 +0300

    Task 010 solved

--- solutions/010.js
+++ solutions/010.js
@@ -1,22 +1,9 @@
 function postTweet(tweetText) {
-  // Я понимаю что данная функция это скорее имитация fetch, но я решил сделать ее скорее оберткой
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-
-      // Поскольку по-факту у нас тут может быть какая-то обработка, которая зафейлиться,
-      // то этот блок тоже стоит оборачивать в try-catch
-      // На Q&A-сессии люди спрашивали когда использовать try-catch а когда then-catch,
-      // просто не понимая что сравнивают теплое с мягким.
-      try {
-        // вот тут у нас как будто вызов функции, которая возвращает промис
-        const isSuccess = Math.random() < 0.9;
-
-        if (isSuccess) {
-          resolve(`Твит '${tweetText}' успешно опубликован!`);
-        } else {
-          
-        }
-      } catch (error) {
+      if (Math.random() < 0.7) {
+        resolve(`Твит '${tweetText}' успешно опубликован!`);
+      } else {
         reject(new Error('Ошибка сети при публикации'));
       }
     }, 1 * 1000 );
@@ -24,5 +11,17 @@ function postTweet(tweetText) {
 }
 
 async function publish() {
-  
+  try {
+    const result = await postTweet('Мой первый твит с async/await!');
+    console.log(result);
+    
+  } catch (error) {
+    console.log(error.message);
+    
+  } finally {
+    console.log('Операция публикации завершена (успех или ошибка)');
+  }
+
 }
+
+publish();
