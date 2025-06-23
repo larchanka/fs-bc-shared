046221265e3a319f6502fd8670735ea8ffd946bd
chaoBambino <JoseMartinesFlores@proton.me>
2025-06-23 19:26:00 +0500

    quiz/009

---
+++ solutions/009.js
@@ -0,0 +1,7 @@
+let config = loadConfig();
+let data = processData(config);
+render(data);
+console.log('Готово!');
+
+Что такое синхронный код?
+- Код, который выполняется строка за строкой, ожидая завершения предыдущей
