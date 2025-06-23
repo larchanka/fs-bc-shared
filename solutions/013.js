8d06876a45b536c982832df24a606f360464426e
chaoBambino <JoseMartinesFlores@proton.me>
2025-06-23 19:27:47 +0500

    quiz/013

---
+++ solutions/013.js
@@ -0,0 +1,10 @@
+const promise = new Promise((resolve, reject) => {
+  reject(new Error('Сервер недоступен'));
+});
+
+promise
+  .then(result => console.log('Успех:', result))
+  .catch(error => console.log('Ошибка сети:', error.message));
+  
+Что выведет код, если Promise будет отклонен?
+- Ошибка сети: Сервер недоступен
