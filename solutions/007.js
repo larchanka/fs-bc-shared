45487d5c5c6aee56cfe65d847b97e19bf2b60756
vadim-troian <vadymtroiyan@gmail.com>
2025-06-23 10:22:37 +0300

    Create 007.js

---
+++ solutions/007.js
@@ -0,0 +1,8 @@
+console.log("Начало загрузки страницы видео");
+new Promise((resolve) => resolve()).then(() => {
+  console.log("Основная информация о видео загружена");
+});
+setTimeout(() => {
+  console.log("Список рекомендаций загружен");
+}, 0);
+console.log("Конец скрипта");
