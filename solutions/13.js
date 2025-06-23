781af5195136bda0aceb791842968b87963850a8
Aliaksei <156023873+azhemoita@users.noreply.github.com>
2025-06-23 12:07:40 +0300

    Create 13.js

---
+++ solutions/13.js
@@ -0,0 +1,11 @@
+const promise = new Promise((resolve, reject) => {
+  reject(new Error('Сервер недоступен'));
+});
+
+promise
+  .then(result => console.log('Успех:', result))
+  .catch(error => console.log('Ошибка сети:', error.message));
+
+// Что выведет код, если Promise будет отклонен?
+// Ответ:
+// Ошибка сети: Сервер недоступен
